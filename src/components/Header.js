5715b2c2b7d5419be9910fc9c0934bd778c2b188
Joshua Rule Dobson <joshua@joshuarule.com>
2021-02-08 09:41:43 -0500

    fix header z-index

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import Icon from "./Icon";
 
 export default function Header() {
   return (
-    <header className="absolute top-0 z-10 left-0 right-0 ">
+    <header className="absolute top-0 z-50 left-0 right-0 ">
       <div className="container py-8">
         <nav className="flex justify-between">
           <div style={{ maxWidth: "200px" }}>
